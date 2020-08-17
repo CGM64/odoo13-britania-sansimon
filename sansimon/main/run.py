66fb588b36f39483b1c0d13393e569bc2d0dc2c3
Juan Carlos Rivera Zamora <juancr83@outlook.com>
2020-08-17 09:46:05 -0600

    [REF] Setear plantilla.

--- sansimon/main/run.py
+++ sansimon/main/run.py
@@ -9,6 +9,8 @@ url = 'http://localhost:8013'
 db = 'Odoo13_SanSimonProd'
 username = 'admin'
 password = 'asdf'
+url = 'http://192.168.99.100:29013'
+db = 'Odoo13_SanSimon'
 datos = []
 common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(url))
 models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(url))
@@ -16,5 +18,7 @@ models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(url))
 uid = common.authenticate(db, username, password, {})
 
 #models.execute_kw(db, uid, password, 'product.template', 'delete_all_date', [[]])
-models.execute_kw(db, uid, password, 'product.template', 'get_import_intelisis', [[]])
-models.execute_kw(db, uid, password, 'stock.inventory', 'getInventoryIntelisis', [[]])
+#models.execute_kw(db, uid, password, 'product.template', 'get_import_intelisis', [[]])
+#models.execute_kw(db, uid, password, 'stock.inventory', 'getInventoryIntelisis', [[]])
+models.execute_kw(db, uid, password, 'account.account.template', 'actualizarPlantillaID', [[]])
+#models.execute_kw(db, uid, password, 'account.chart.template', 'try_loading', [[]])
