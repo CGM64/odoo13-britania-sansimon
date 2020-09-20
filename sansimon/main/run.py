47c6417bc29111db01e752898ad1691d45b8470f
Juan Carlos Rivera Zamora <juancr@outlook.com>
2020-09-20 10:31:40 -0600

    [REF] .

--- sansimon/main/run.py
+++ sansimon/main/run.py
@@ -20,18 +20,14 @@ uid = common.authenticate(db, username, password, {})
 
 #Sirve para setear un valor en la configuracion de la conta para volver a cargar la plantilla contable
 #models.execute_kw(db, uid, password, 'account.account.template', 'actualizarPlantillaID', [[]])
+#models.execute_kw(db, uid, password, 'account.chart.template', 'try_loading', [[]])
 
 #Sirve para cargar el catalogo de productos
 #models.execute_kw(db, uid, password, 'product.template', 'delete_all_date', [[]])
-models.execute_kw(db, uid, password, 'product.template', 'get_import_intelisis', [[]])
+#models.execute_kw(db, uid, password, 'product.template', 'get_import_intelisis', [[]])
 
 #Sirve para cargar el inventario de instelisis
 #models.execute_kw(db, uid, password, 'stock.inventory', 'getInventoryIntelisis', [[]])
 
 #Sirve para validar todos los movimientos de ajuste
-#models.execute_kw(db, uid, password, 'stock.inventory', 'validateStockInventory', [[]])
-
-
-
-
-#models.execute_kw(db, uid, password, 'account.chart.template', 'try_loading', [[]])
+models.execute_kw(db, uid, password, 'stock.inventory', 'validateStockInventory', [[]])
