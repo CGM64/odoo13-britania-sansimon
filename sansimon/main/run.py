b59b2e5cdf5803df2ae85f35883855d9d384358d
Juan Carlos Rivera Zamora <juancr83@outlook.com>
2021-01-02 01:26:27 -0600

    .

--- sansimon/main/run.py
+++ sansimon/main/run.py
@@ -24,10 +24,10 @@ uid = common.authenticate(db, username, password, {})
 
 #Sirve para cargar el catalogo de productos
 #models.execute_kw(db, uid, password, 'product.template', 'delete_all_date', [[]])
-models.execute_kw(db, uid, password, 'product.template', 'get_import_intelisis', [[]])
+#models.execute_kw(db, uid, password, 'product.template', 'get_import_intelisis', [[]])
 
 #Sirve para cargar el inventario de instelisis
 #models.execute_kw(db, uid, password, 'stock.inventory', 'getInventoryIntelisis', [[]])
 
 #Sirve para validar todos los movimientos de ajuste
-#models.execute_kw(db, uid, password, 'stock.inventory', 'validateStockInventory', [[]])
+models.execute_kw(db, uid, password, 'stock.inventory', 'validateStockInventory', [[]])
