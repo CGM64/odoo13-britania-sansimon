06964faea98891c0da52a34e9880ae1a078aa953
juancr83 <juancr83@outlook.com>
2021-03-13 12:01:49 -0600

    [REF] Agregando columnas de categoria.

--- sansimon/wizard/stock_move_periodo_report_xls.py
+++ sansimon/wizard/stock_move_periodo_report_xls.py
@@ -14,23 +14,46 @@ class StockMovePeriodoXls(models.AbstractModel):
     def generate_xlsx_report(self, workbook, data, data_report):
         self.workbook = workbook
         sheet_libro = workbook.add_worksheet('Inventario')
-        productos = self.env['product.product'].search([('active','=',True),('qty_available','>',0)])
+
         i = 0;
         sheet_libro.write(i,0,"Linea")
         sheet_libro.write(i,1,"Codigo")
         sheet_libro.write(i,2,"Descripcion")
-        sheet_libro.write(i,3,"Cantidad")
-        sheet_libro.write(i,4,"qty_available")
-        sheet_libro.write(i,5,"incoming_qty")
-        sheet_libro.write(i,6,"outgoing_qty")
+        sheet_libro.write(i,3,"Ext. Actual")
+        sheet_libro.write(i,4,"Cnt. Ini.")
+        sheet_libro.write(i,5,"Entradas")
+        sheet_libro.write(i,6,"Salidas")
+        sheet_libro.write(i,7,"Cnt. Final")
         sheet_libro.set_column(0,0,5)
         sheet_libro.set_column(1,1,16)
         sheet_libro.set_column(2,2,60)
-        sheet_libro.set_column(3,6,12)
+        sheet_libro.set_column(3,7,12)
+
+
+
+
+        productos = self.env['product.product'].search([
+                ('active','=',True),
+                ('qty_available','>',0),
+                #('id','=',1826),
+                ])
         productos = productos.filtered(lambda p: p.type != 'service')
-        print(data)
-        res = productos._compute_quantities_dict(None, None, None, data['form']['fecha_inicio'], data['form']['fecha_fin'])
+        #print(data)
+        fecha_inicio = data['form']['fecha_inicio']
+        fecha_fin = data['form']['fecha_fin']
+        res = productos._compute_quantities_dict(None, None, None, fecha_inicio, fecha_inicio)
+
+
         for producto in productos:
+            Move = self.env['stock.move']
+            domain_quant_loc, domain_move_in_loc, domain_move_out_loc = producto.with_context(company_owned=True)._get_domain_locations()
+            domain_move_in = [('product_id', '=', producto.id),('state', '=', 'done'), ('date', '>=', fecha_inicio), ('date', '<=', fecha_fin)] + domain_move_in_loc
+            domain_move_out = [('product_id', '=', producto.id),('state', '=', 'done'), ('date', '>=', fecha_inicio), ('date', '<=', fecha_fin)] + domain_move_out_loc
+
+            moves_in_res = dict((item['product_id'][0], item['product_qty']) for item in Move.read_group(domain_move_in, ['product_id', 'product_qty'], ['product_id'], orderby='id'))
+            moves_out_res = dict((item['product_id'][0], item['product_qty']) for item in Move.read_group(domain_move_out, ['product_id', 'product_qty'], ['product_id'], orderby='id'))
+
+
             i+=1
             sheet_libro.write(i,0,i)
             sheet_libro.write(i,1,producto.default_code)
@@ -38,10 +61,30 @@ class StockMovePeriodoXls(models.AbstractModel):
             sheet_libro.write(i,3,producto.qty_available)
 
             sheet_libro.write(i,4,res[producto.id]['qty_available'])
-            sheet_libro.write(i,5,res[producto.id]['incoming_qty'])
-            sheet_libro.write(i,6,res[producto.id]['outgoing_qty'])
+            sheet_libro.write(i,5,moves_in_res.get(producto.id, 0.0))
+            sheet_libro.write(i,6,moves_out_res.get(producto.id, 0.0))
+
+            cantidad_final = res[producto.id]['qty_available'] + moves_in_res.get(producto.id, 0.0) - moves_out_res.get(producto.id, 0.0)
+            sheet_libro.write(i,7,cantidad_final)
+
+            sheet_libro.write(i,8,producto.categ_id.name)
+
+            lista = self.categoria(producto.categ_id.id)
+            j=7
+            for categoria in lista:
+                j += 1
+                sheet_libro.write(i,j,categoria)
 
 
+    def categoria(self, categoria_id):
+        lista = list()
+        categorias = self.env['product.category'].search([('id','=',categoria_id)])
+        for categoria in categorias:
+
+            lista.append(categoria.name)
+            if categoria.parent_id:
+                lista = self.categoria(categoria.parent_id.id) + lista
+        return lista
 
 
 
