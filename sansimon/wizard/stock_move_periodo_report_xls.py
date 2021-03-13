618ee616e901793c2879bc5e79ef884337863294
juancr83 <juancr83@outlook.com>
2021-03-12 22:36:47 -0600

    [REF] Report v1

--- sansimon/wizard/stock_move_periodo_report_xls.py
+++ sansimon/wizard/stock_move_periodo_report_xls.py
@@ -14,7 +14,35 @@ class StockMovePeriodoXls(models.AbstractModel):
     def generate_xlsx_report(self, workbook, data, data_report):
         self.workbook = workbook
         sheet_libro = workbook.add_worksheet('Inventario')
-        sheet_libro.write(0,0,'Hola Mundo')
+        productos = self.env['product.product'].search([('active','=',True),('qty_available','>',0)])
+        i = 0;
+        sheet_libro.write(i,0,"Linea")
+        sheet_libro.write(i,1,"Codigo")
+        sheet_libro.write(i,2,"Descripcion")
+        sheet_libro.write(i,3,"Cantidad")
+        sheet_libro.write(i,4,"qty_available")
+        sheet_libro.write(i,5,"incoming_qty")
+        sheet_libro.write(i,6,"outgoing_qty")
+        sheet_libro.set_column(0,0,5)
+        sheet_libro.set_column(1,1,16)
+        sheet_libro.set_column(2,2,60)
+        sheet_libro.set_column(3,6,12)
+        productos = productos.filtered(lambda p: p.type != 'service')
+        print(data)
+        res = productos._compute_quantities_dict(None, None, None, data['form']['fecha_inicio'], data['form']['fecha_fin'])
+        for producto in productos:
+            i+=1
+            sheet_libro.write(i,0,i)
+            sheet_libro.write(i,1,producto.default_code)
+            sheet_libro.write(i,2,producto.name)
+            sheet_libro.write(i,3,producto.qty_available)
+
+            sheet_libro.write(i,4,res[producto.id]['qty_available'])
+            sheet_libro.write(i,5,res[producto.id]['incoming_qty'])
+            sheet_libro.write(i,6,res[producto.id]['outgoing_qty'])
+
+
+
 
 
         # Requests = self.env['hr.leave']
