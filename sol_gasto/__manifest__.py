fe8423a98d4530daa069fdb4689942694ac6b355
Juan Carlos Rivera Zamora <juancr83@outlook.com>
2021-04-21 15:32:02 -0600

    [REF] Completar.....

--- sol_gasto/__manifest__.py
+++ sol_gasto/__manifest__.py
@@ -25,8 +25,7 @@
     # always loaded
     'data': [
         'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'views/gs_gastos_views.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
