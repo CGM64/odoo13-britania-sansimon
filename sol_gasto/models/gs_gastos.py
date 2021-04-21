50a0736a866b417e97911b145a799ce58ea9245d
jhayrmarin <jhayr.marin95@gmail.com>
2021-04-21 12:20:43 -0600

    boton vista

--- sol_gasto/models/gs_gastos.py
+++ sol_gasto/models/gs_gastos.py
@@ -8,3 +8,7 @@ from odoo.tools import email_split, float_is_zero
 class GsGastos(models.Model):
     _name = "gs.gastos"
     _description = "Gastos"
+
+    name=fields.Char(string="Descripcion")
+    date=fields.Datetime(string="Fecha")
+    done=fields.Boolean(string="Realizada")
