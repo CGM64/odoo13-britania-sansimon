6cb331ac66f33429a5daadc62374bb89ea56213e
jhayrmarin <jhayr.marin95@gmail.com>
2021-04-21 15:15:00 -0600

    Modificaciones

--- sol_gasto/models/gs_gastos.py
+++ sol_gasto/models/gs_gastos.py
@@ -9,6 +9,12 @@ class GsGastos(models.Model):
     _name = "gs.gastos"
     _description = "Gastos"
 
-    name=fields.Char(string="Descripcion")
-    date=fields.Datetime(string="Fecha")
-    done=fields.Boolean(string="Realizada")
+
+    name = fields.Char('Description', readonly=True, required=True, states={'draft': [('readonly', False)], 'reported': [('readonly', False)], 'refused': [('readonly', False)]})
+    state = fields.Selection([
+        ('draft', 'To Submit'),
+        ('reported', 'Submitted'),
+        ('approved', 'Approved'),
+        ('done', 'Paid'),
+        ('refused', 'Refused')
+    ], compute='_compute_state', string='Status', copy=False, index=True, readonly=True, store=True, help="Status of the expense.")
