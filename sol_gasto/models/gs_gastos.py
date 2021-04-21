b47d8d09ee92c06cfb0ff65afa3aac19b235e2f2
jhayrmarin <jhayr.marin95@gmail.com>
2021-04-21 11:01:05 -0600

    Creación del modelo gs_gastos

---
+++ sol_gasto/models/gs_gastos.py
@@ -0,0 +1,10 @@
+import re
+
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError, ValidationError
+from odoo.tools import email_split, float_is_zero
+
+
+class GsGastos(models.Model):
+    _name = "gs.gastos"
+    _description = "Expense"
