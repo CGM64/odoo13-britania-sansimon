57825389316a47e9abcb31d9a4946479258f6299
jhayrmarin <jhayr.marin95@gmail.com>
2021-05-05 17:08:26 -0600

    Versión funcional sin monto

--- sol_gasto/models/gs_gastos.py
+++ sol_gasto/models/gs_gastos.py
@@ -135,31 +135,29 @@ class GsComprasGastos(models.Model):
 
     def aplicar_pago(self):
         proveedor = self.partner_id.id
-        moneda = self.currency_id.id
-        monto = self.amount_total
-        context = {
-            'active_model': 'purchase.order',
-            'readonly': True,
+        monto = round(self.amount_total, 2)
+
+        self.env.context = dict(self.env.context)
+        self.env.context.update({
             'default_communication': self.name,
             'default_payment_type': 'outbound',
             'default_partner_type': 'supplier',
-            'default_amount': monto,
+            'default_amount': abs(self.amount_total),
             'default_partner_id': proveedor,
             'default_payment_method_id': 4,
             'default_journal_id': 56,
-            'default_currency_id': moneda,
             'default_check_no_negociable': 1,
-        }
+        })
+        
         self.ensure_one()
-        print("Context----------->", context)
         return {
             'name': 'Registrar Pago',
             'res_model': 'account.payment',
             'view_mode': 'form',
-            'view_id': 'account.view_account_payment_invoice_form',
+            'view_id': self.env.ref('account.view_account_payment_invoice_form').id,
             'target': 'new',
-            'context': context,
+            'context': self.env.context,
             'view_type': 'form',
-            'views': [(False, 'form')],
+            #'views': [(False, 'form')],
             'type': 'ir.actions.act_window',
         }
