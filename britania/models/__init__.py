da904916d19099dd2299f81cf993c5c4d6e56994
Miguel <miguel.angelgt2010@gmail.com>
2021-07-07 11:55:22 -0600

    [REF] Se agrego metodo default a repair.order para repair.type

--- britania/models/__init__.py
+++ britania/models/__init__.py
@@ -5,4 +5,3 @@ from . import fleet_repair
 from . import repair_order
 from . import crm
 from . import sale_order
-from . import repair_type
--- britania/models/repair_order.py
+++ britania/models/repair_order.py
@@ -3,9 +3,41 @@
 from odoo import api, fields, models, _
 from odoo.exceptions import ValidationError
 from odoo.http import request
+           
+class RepairType(models.Model):
+    _name = "repair.type"
+    _description = "Tipos de ordenes de reparación"
+
+    name = fields.Char('Tipo de orden', required=True)
+    sequence = fields.Integer(help='Used to order repair types in the dashboard view', default=10)
+    active = fields.Boolean(string ="Activo",default=True, help="Activa o desctiva tipos de orden")
+    
+    state = fields.Selection([
+        ('borrador', 'Borrador'),
+    ], default='borrador', string='Estado', copy=False, index=True, readonly=True, help="Estado de la Conversión.")
+    
+    @api.onchange('active')
+    def _update_active(self):
+        return
+
+    _sql_constraints = [
+        ('name_uniq', 'unique (name)', "Ya existe un registro con este nombre!"),
+    ]
 
 class repairOrder(models.Model):
     _inherit = "repair.order"
+
+    @api.model
+    def _get_default(self):
+        type = self.env['repair.type'].search([('active', '=',True)])
+        tipos = []
+        for tipo in type:
+            tipos.append(tipo.sequence)
+        minimo = min(tipos)
+        t = self.env['repair.type'].search([('sequence', '=',minimo)]).id
+        return t
+    
+    tipo_orden = fields.Many2one('repair.type', 'Tipo de orden', index=True, default=_get_default)
 
     def update_detail(self, search=''):
 
