134b6407a8f563919af99dcdc3a9c6f2fae92427
JhayrMarin <jhayr.marin95@gmail.com>
2021-07-08 12:20:47 -0600

    [correcciones]

--- corte_caja/__manifest__.py
+++ corte_caja/__manifest__.py
@@ -27,11 +27,8 @@
         'views/account_payment_corte_caja_view.xml',
         'sequence/corte_caja_sequence.xml',  
 
-        'report/account_payment_corte_caja_report_views.xml',  
         'report/corte_caja_report_pdf.xml',  
-        
-        
-             
+        'report/account_payment_corte_caja_report_views.xml',       
     ],
     # only loaded in demonstration mode
     'demo': [
