e498d9e65b72bb4c611ab8c969f30998daf51523
JhayrMarin <jhayr.marin95@gmail.com>
2021-08-25 14:30:19 -0600

    [Validacion] validando el porcentaje al momento de publicar la factura.

--- sansimon/models/sale_order.py
+++ sansimon/models/sale_order.py
@@ -16,17 +16,46 @@ class CrmTeamInherit(models.Model):
 class SaleOrderInherit(models.Model):
     _inherit = 'sale.order'
 
-
     def action_confirm(self):
-        # order = super(SaleOrderInherit,self).action_confirm()
         porcentaje_maximo=self.team_id.porcentaje_maximo
         cont=0
         for line in self.order_line:
             if line.discount >porcentaje_maximo:
                 cont+=1
         
-        print('Porcentaje Team->',porcentaje_maximo,' Contador',cont,' Team',self.team_id.name)
         if cont ==0:
             return super(SaleOrderInherit,self).action_confirm()
         else:
-            raise UserError(_("El porcentaje de descuento es mayo al porcentaje permitido."))
+            raise UserError(_("El porcentaje de descuento es mayor al porcentaje permitido."))
+
+
+class AccountMoveInherit(models.Model):
+    _inherit = 'account.move'
+
+    def action_post(self):
+        porcentaje_maximo=self.team_id.porcentaje_maximo
+        cont=0
+        for line in self.invoice_line_ids:
+            if line.discount >porcentaje_maximo:
+                cont+=1
+        
+        if cont ==0:
+            return super(AccountMoveInherit,self).action_post()
+        else:
+            raise UserError(_("El porcentaje de descuento es mayor al porcentaje permitido."))
+
+
+# class SaleAdvancePaymentInv(models.TransientModel):
+#     _inherit = "sale.advance.payment.inv"
+
+#     def create_invoices(self):
+
+#         porcentaje_maximo=self.team_id.porcentaje_maximo
+#         cont=0
+#         for line in self.order_line:
+#             if line.discount >porcentaje_maximo:
+#                 cont+=1
+#         if cont ==0:
+#             return super(SaleAdvancePaymentInv,self).create_invoices()
+#         else:
+#             raise UserError(_("El porcentaje de descuento es mayo al porcentaje permitido."))
