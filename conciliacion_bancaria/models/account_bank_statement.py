7e3972ed5a4a5476278c2a4f48293f2dbf4c3af7
JhayrMarin <jhayr.marin95@gmail.com>
2021-09-27 15:19:17 -0600

    [cambio] generando arreglo BAM

--- conciliacion_bancaria/models/account_bank_statement.py
+++ conciliacion_bancaria/models/account_bank_statement.py
@@ -1,5 +1,13 @@
 # -*- coding: utf-8 -*-
+import re
+
 from odoo import api, fields, models, _
+from odoo.exceptions import AccessError, UserError, RedirectWarning, ValidationError, Warning
+from odoo.tools import email_split, float_is_zero
+from odoo.addons import decimal_precision as dp
+from odoo.http import request
+import datetime
+
 
 class PrintBankStatement(models.Model):
     _inherit = 'account.bank.statement'
@@ -18,6 +26,68 @@ class PrintBankStatement(models.Model):
     balance_cargos = fields.Monetary('Cargos', compute='_cargos_abonos_balance')
 
 
-    def conciliacion_bancaria(self):
-        # self
-        return 0
+    def conciliacion_bancaria(self,):
+        account_bank_statement=request.env['account.bank.statement'].search([('id','=',self.id)])
+        account_move_line=request.env['account.move.line'].search([('id','=',self.id)])
+
+        if account_bank_statement.journal_id.bank_id.template_extractos =='BAM':
+            conciliacion=self._conciliacion_BAM(account_bank_statement,account_move_line)
+
+        return conciliacion
+
+    def _conciliacion_BAM(self,account_bank_statement,account_move_line):
+        BAM=['CHEQUE','NC','ND','DEPOSITO']
+        
+        conciliacion={
+                'banco':account_bank_statement.name,
+                'diario':account_bank_statement.journal_id.name,
+                'fecha':account_bank_statement.date,
+                'saldo_inicial':account_bank_statement.balance_start,
+                'saldo_final':account_bank_statement.balance_end_real,
+                'moneda':account_bank_statement.currency_id.symbol,
+            }
+        
+        lista_ids=[]
+        lista_otros=[]
+        lista_conciliacion=[]
+        cheques_circulacion=[]
+        circulacion=0
+        pagados=0
+        for documento in BAM:
+            lista_documento=[]
+            suma_documento=0
+
+            for line in account_bank_statement.line_ids.filtered(lambda l: l.name.startswith(documento)):
+                account_move_line=request.env['account.move.line'].search([('statement_line_id','=',line.id),('date','<=',line.date)])                   
+                if account_move_line.date==False:
+                    suma_documento+=line.amount
+                    if documento=='CHEQUE':
+                        circulacion+=line.amount
+                        cheques_circulacion.append({'line_fecha':line.date,'line_name':line.name,'line_amount':line.amount,'conciliado':'N'})
+                    else:
+                        lista_documento.append({'line_fecha':line.date,'line_name':line.name,'line_amount':line.amount,'conciliado':'N'})
+                else:
+                    suma_documento+=line.amount
+                    if documento=='CHEQUE':
+                        pagados+=line.amount 
+                    lista_documento.append({'line_fecha':line.date,'line_name':line.name,'line_amount':line.amount,'conciliado':'S'})                                    
+
+                if line.id not in lista_ids:
+                    lista_ids.append(line.id)
+
+            conciliacion[documento]=lista_documento
+            conciliacion['ch_circulacion']=circulacion
+            conciliacion['ch_pagados']=pagados
+            conciliacion['CIRCULACION']=cheques_circulacion
+
+        for line in account_bank_statement.line_ids.filtered(lambda l: l.id not in lista_ids):
+            account_move_line=request.env['account.move.line'].search([('statement_line_id','=',line.id)])
+            if account_move_line.date==False:
+                lista_otros.append({'line_fecha':line.date,'line_name':line.name,'line_amount':line.amount,'conciliado':'N'})
+            else:
+                lista_otros.append({'line_fecha':line.date,'line_name':line.name,'line_amount':line.amount,'conciliado':'S'})     
+        conciliacion['otros']=lista_otros
+        lista_conciliacion.append(conciliacion)
+        print('conciliacion>',conciliacion)
+
+        return lista_conciliacion
