7927a537278949ed37a750643a0631e639460ea5
Henry Galvez <soulofhenry@gmail.com>
2023-07-11 16:01:42 -0600

    [REF] stock actual, nombre de columnas

--- britania/models/stock_quant.py
+++ britania/models/stock_quant.py
@@ -3,7 +3,7 @@ from odoo import fields, models, _
 class StockQuant(models.Model):
     _inherit = 'stock.quant'
 
-    product_default_code = fields.Char(related="product_id.default_code")
+    product_default_code = fields.Char(related="product_id.default_code", string="Referencia interna")
     product_chasis = fields.Char(related="product_id.chasis")
-    product_standard_price = fields.Float(related="product_tmpl_id.standard_price")
-    product_name = fields.Char(related="product_tmpl_id.name")
+    product_standard_price = fields.Float(related="product_tmpl_id.standard_price", string="Costo unitario")
+    product_name = fields.Char(related="product_tmpl_id.name", string="Nombre")
