25d1148c7ce6b77119542f31b69532f9fc534945
Henry Galvez <soulofhenry@gmail.com>
2023-07-11 15:55:24 -0600

    [REF] stock actual, nombre de producto sin codigo

--- britania/models/stock_quant.py
+++ britania/models/stock_quant.py
@@ -6,3 +6,4 @@ class StockQuant(models.Model):
     product_default_code = fields.Char(related="product_id.default_code")
     product_chasis = fields.Char(related="product_id.chasis")
     product_standard_price = fields.Float(related="product_tmpl_id.standard_price")
+    product_name = fields.Char(related="product_tmpl_id.name")
