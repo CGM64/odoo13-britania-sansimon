640dd041eda4a0747d9e4a9dbb6efe2d5595b485
Henry Galvez <soulofhenry@gmail.com>
2023-07-12 17:03:57 -0600

    [REF] vista stock actual

--- britania/models/stock_quant.py
+++ britania/models/stock_quant.py
@@ -1,9 +1,10 @@
 from odoo import fields, models, _
 
 class StockQuant(models.Model):
-    _inherit = 'stock.quant'
+    _inherit = 'stock.valuation.layer'
 
     product_default_code = fields.Char(related="product_id.default_code", string="Referencia interna")
     product_chasis = fields.Char(related="product_id.chasis")
     product_standard_price = fields.Float(related="product_tmpl_id.standard_price", string="Costo unitario")
     product_name = fields.Char(related="product_tmpl_id.name", string="Nombre")
+    product_uom = fields.Many2one(related="product_tmpl_id.uom_id", string="Nombre")
