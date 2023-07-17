71ea962418d9d97ea68171f94613d7cec3b5b978
Henry Galvez <soulofhenry@gmail.com>
2023-07-17 09:21:12 -0600

    [REF] Corrigiendo etiqueta de unidades de medida en vista stock actual

--- britania/models/stock_quant.py
+++ britania/models/stock_quant.py
@@ -7,4 +7,4 @@ class StockQuant(models.Model):
     product_chasis = fields.Char(related="product_id.chasis")
     product_standard_price = fields.Float(related="product_tmpl_id.standard_price", string="Costo unitario")
     product_name = fields.Char(related="product_tmpl_id.name", string="Nombre")
-    product_uom = fields.Many2one(related="product_tmpl_id.uom_id", string="Nombre")
+    product_uom = fields.Many2one(related="product_tmpl_id.uom_id", string="Unidad de medida")
